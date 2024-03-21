1e08ea57c08a845f9ccee7ebd48dea02d920af77
JiHongKim98 <kinjihong9598@gmail.com>
2024-03-21 23:59:18 +0900

    24/03/21

---
+++ Lv.1/약수의-합.java
@@ -0,0 +1,22 @@
+/*
+ * 24/03/21
+ * 
+ * 프로그래머스 - 약수의 합
+ * 
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12928
+ */
+
+// 내 풀이
+class Solution_Me {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                answer += i;
+			}
+        }
+        
+        return answer;
+    }
+}
