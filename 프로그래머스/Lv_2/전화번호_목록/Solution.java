072b68d6f1313a92542a89684bf8a4477fff3cd2
JiHongKim98 <kinjihong9598@gmail.com>
2025-08-10 00:12:07 +0900

    프로그래머스: 전화번호 목록

---
+++ 프로그래머스/Lv_2/전화번호_목록/Solution.java
@@ -0,0 +1,21 @@
+/*
+ * 프로그래머스 - 전화번호 목록
+ *
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42577
+ */
+package Lv_2.전화번호_목록;
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i].length() > phone_book[i + 1].length()) continue;
+
+            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
+        }
+        return true;
+    }
+}
